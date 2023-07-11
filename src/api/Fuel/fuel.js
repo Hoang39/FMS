884167027965af7a18f20af3c3122314161f87e9
Nguyễn Huy Hoàng <zmeb030902@gmail.com>
2023-07-11 10:18:12 +0700

    first call api

--- src/api/Fuel/fuel.js
+++ src/api/Fuel/fuel.js
@@ -1,17 +1,37 @@
 import axios from "axios";
 
+const encodeFormData = (params={}) => {
+  return Object.keys(params).map(
+    (key) =>
+      `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+    )
+    .join("&")
+}
+
 export const getVehiclesList = async (token) => {
   try {
-    const res = await axios({
-      method: "get",
-      url: "http://testv4.adagps.com/index.php/api/FuelExtendedChangeController/getVehiclesList",
-      headers: {
-        Authorization: `Bearer ${token}`
-      },
-    });
+    // const res = await axios({
+    //   method: "post",
+    //   url: "http://testv4.adagps.com/index.php/api/FuelExtendedChangeController/getVehiclesList",
+    //   headers: {
+    //     Authorization: `bearer ${token}`,
+    //   },
+    //   params: encodeFormData({
+    //     Authentication: `${token}`,
+    //   })
+    // });
+    const res = await axios.post(
+      'http://testv4.adagps.com/index.php/api/FuelExtendedChangeController/getVehiclesList',
+      encodeFormData({Authentication: token}),
+      {
+        headers: {
+          Authorization: `bearer ${token}`
+        }
+      }
+    )
     return res.data;
   } catch(error) {
-    console.log(error)
-    return error.response.data;
+    console.log('error', error)
+    return [];
   }
 };
